fb868d53e8f8a92cb64e87dd2db7b6b00dca6ad5
khano <khano@DESKTOP-8T88M38>
2020-04-06 15:57:29 +0900

    First commit

---
+++ src/Printing.java
@@ -0,0 +1,9 @@
+
+public class Printing {
+
+	public static void main(String[] args) {
+		System.out.print("Working perfectly");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author khano
+ *
+ */
+module JenkinConnectionProject {
+}
